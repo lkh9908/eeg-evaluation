9e6d07f8e46966743d9970c835c379949cfef978
kaihao.liu <kaihao.liu@neurogin.com>
2021-07-26 17:55:40 +0800

    uploads

--- preprocessor.py
+++ preprocessor.py
@@ -41,7 +41,8 @@ class Cleaner(object):
         #divide into epochs
         new_events = mne.make_fixed_length_events(self.raw, duration=2.)
         event_dict = {'divide':1}
-        
+        print('i wanna dieeeeeeeeeeeee')
+        print(len(new_events))
         #reject data with extreme/flat amplitude
         reject_criteria = {'eeg' : 400e-6}       # 400 µV
         flat_criteria = {'eeg' : 1e-6}          # 1 µV
